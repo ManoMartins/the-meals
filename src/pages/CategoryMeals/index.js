43e68822ec5127d40276221d5ea85dc957d5e5e8
Manoel Martins <mano.martins29@gmail.com>
2020-04-29 23:18:26 -0300

    feat: Screen of meals

--- src/pages/CategoryMeals/index.js
+++ src/pages/CategoryMeals/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
+import { Text, View } from 'react-native';
 
-import { CATEGORIES } from '../../data/dummy-data';
+import { CATEGORIES, MEALS } from '../../data/dummy-data';
 
-import { Container } from './styles';
+import { Container, Content, MealsButton, MealItem, MealHeader, MealHeaderBackground, MealHeaderTitle, MealDetail, MealDetailText } from './styles';
 
 const CategoryMeals = ({ route, navigation }) => {
   const { categoryId } = route.params;
   const category = CATEGORIES.find(cate => cate.id === categoryId);
+  const displayMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(categoryId) >= 0
+  );
 
   navigation.setOptions({ title: category.title })
 
   return (
     <Container>
-      <Text>{category.title}</Text>
-      <Button
-        title="Test"
-        onPress={() => navigation.setOptions({title: 'oi'})}
+      <Content
+        data={displayMeals}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => (
+          <MealItem>
+            <MealsButton>
+              <View>
+                <MealHeader>
+                  <MealHeaderBackground source={{uri: item.imageUrl}}>
+                    <MealHeaderTitle>{item.title}</MealHeaderTitle>
+                  </MealHeaderBackground>
+                </MealHeader>
+                <MealDetail>
+                  <MealDetailText>{item.duration}m</MealDetailText>
+                  <MealDetailText>{item.complexity.toUpperCase()}</MealDetailText>
+                  <MealDetailText>{item.affordability.toUpperCase()}</MealDetailText>
+                </MealDetail>
+              </View>
+            </MealsButton>
+          </MealItem>
+        )}
       />
     </Container>
   );
--- src/pages/CategoryMeals/styles.js
+++ src/pages/CategoryMeals/styles.js
@@ -1,7 +1,57 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding: 15px;
+`;
+
+export const Content = styled.FlatList`
+  width: 100%;
+`;
+
+export const MealsButton = styled.TouchableOpacity``;
+
+export const MealItem = styled.View`
+  margin: 10px 0;
+  height: 200px;
+  width: 100%;
+  background-color: #340068;
+  border-radius: 10px;
+  overflow: hidden;
+`;
+
+
+export const MealHeader = styled.View`
+  flex-direction: row;
+  height: 85%;
+`;
+
+export const MealHeaderBackground = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+  justify-content: flex-end;
+`;
+
+export const MealHeaderTitle = styled.Text`
+  font-family: 'ComicNeue-Bold';
+  font-size: 22px;
+  color: #FFD9DA;
+  background-color: rgba(0, 0, 0, 0.36);
+  padding: 5px 12px;
+  text-align: center;
+`;
+
+export const MealDetail = styled.View`
+  flex-direction: row;
+  padding: 0 10px;
+  justify-content: space-between;
+  align-items: center;
+  height: 15%;
+`;
+
+export const MealDetailText = styled.Text`
+  font-family: 'ComicNeue-Bold';
+  color: #FFD9DA;
 `;
